cb8215fe6b0c00483a4780f908735ffcf9c5edaa
Vexx <36227512+Pozitrone@users.noreply.github.com>
2018-06-14 11:08:25 +0200

    Linked CSS file and added W3schools switch rules.

--- graphic.php
+++ graphic.php
@@ -4,8 +4,12 @@
         <title>Graphic Form Generator</title> 
         <meta lang="en">
         <meta charset="utf-8">
+        <link href="graphic.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-        
+        <label class="switch">
+            <input type="checkbox">
+            <span class="slider round"></span>
+        </label>
     </body>
 </html>
