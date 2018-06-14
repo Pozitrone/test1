d5a1595abc0182c6b6637a41679547cd170a413f
Vexx <36227512+Pozitrone@users.noreply.github.com>
2018-06-14 11:00:46 +0200

    Added basic head of HTML.

---
+++ graphic.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Graphic Form Generator</title> 
+        <meta lang="en">
+        <meta charset="utf-8">
+    </head>
+    <body>
+        
+    </body>
+</html>
