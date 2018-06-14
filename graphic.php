d9ecd8cc455c950735994bb90783f930f13ce4cc
Vexx <36227512+Pozitrone@users.noreply.github.com>
2018-06-14 11:56:32 +0200

    Added first fieldset.

--- graphic.php
+++ graphic.php
@@ -7,9 +7,37 @@
         <link href="graphic.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-        <label class="switch">
-            <input type="checkbox">
-            <span class="slider round"></span>
-        </label>
+        <form>
+            <fieldset>
+
+                <label for="selectFormMethod">form method</label>
+                <select name="selectFormMethod">
+                    <option>GET</option>
+                    <option selected>POST</option>
+                </select>
+
+                <label for="textFormAction">form action</label>
+                <input type="text" name="textFormAction">
+
+                <label for="textFormId">form id</label>
+                <input type="text" name="textFormId">
+
+                <label for="numberFormWidth">form width</label>
+                <input type="number" name="numberFormWidth">
+                <select name="selectFormWidth">
+                    <option>px</option>
+                    <option>em</option>
+                    <option>%</option>
+                </select>
+
+
+            </fieldset>
+
+            <label class="switch">
+                <input type="checkbox" name="switchUsePlaceholders">
+                <span class="slider round"></span>
+            </label>
+        </form>
+        
     </body>
 </html>
