065ac1881838c1145128baae62c5bb2741369d9a
Vexx <36227512+Pozitrone@users.noreply.github.com>
2018-06-15 09:56:29 +0200

    Added another fieldset.

--- graphic.php
+++ graphic.php
@@ -33,36 +33,74 @@
             </fieldset>
             
             <fieldset>
-                <label for="switchUsePlaceholders">use placeholders</label>
-                <label class="switch">
-                    <input type="checkbox" name="switchUsePlaceholders">
-                    <span class="slider round"></span>
-                </label>
+                <div>
+                    <label for="switchUsePlaceholders">use placeholders</label>
+                    <label class="switch">
+                        <input type="checkbox" name="switchUsePlaceholders">
+                        <span class="slider round"></span>
+                    </label>
+                </div>
                 
-                <label for="switchUseCustomCSS">use custom css</label>
-                <label class="switch">
-                    <input type="checkbox" name="switchUseCustomCSS">
-                    <span class="slider round"></span>
-                </label>
+                <div>
+                    <label for="switchUseCustomCSS">use custom css</label>
+                    <label class="switch">
+                        <input type="checkbox" name="switchUseCustomCSS">
+                        <span class="slider round"></span>
+                    </label>
+                </div>
                 
-                <label for="switchUseReset">use reset</label>
-                <label class="switch">
-                    <input type="checkbox" name="switchUseReset">
-                    <span class="slider round"></span>
-                </label>
+                <div>
+                    <label for="switchUseReset">use reset</label>
+                    <label class="switch">
+                        <input type="checkbox" name="switchUseReset">
+                        <span class="slider round"></span>
+                    </label>
+                </div>
+                    
+                <div>
+                    <label for="switchUseCustomColors">use custom colors</label>
+                    <label class="switch">
+                        <input type="checkbox" name="switchUseCustomColors">
+                        <span class="slider round"></span>
+                    </label>
+                </div>
                 
-                <label for="switchUseCustomColors">use custom colors</label>
-                <label class="switch">
-                    <input type="checkbox" name="switchUseCustomColors">
-                    <span class="slider round"></span>
-                </label>
-                
-                <label for="switchUseFieldsets">use fieldsets</label>
-                <label class="switch">
-                    <input type="checkbox" name="switchUseFieldsets">
-                    <span class="slider round"></span>
-                </label>
+                <div>
+                    <label for="switchUseFieldsets">use fieldsets</label>
+                    <label class="switch">
+                        <input type="checkbox" name="switchUseFieldsets">
+                        <span class="slider round"></span>
+                    </label>
+                </div>
             
+            </fieldset>
+            
+            
+            <fieldset>
+                
+                <label for="textSubmitValue">submit value</label>
+                <input type="text" name="textSubmitValue">
+            
+                <label for="textResetValue">reset value</label>
+                <input type="text" name="textResetValue">
+            
+                <label for="numberInputAmount">input amount</label>
+                <input type="number" name="numberInputAmount">
+            
+                <label for="textCssPath">css path</label>
+                <input type="text" name="textCssPath">
+            
+                <label for="selectPreset">preset</label>
+                <select name="selectPreset">
+                    <option selected>Default</option>
+                    <option>smooth</option>
+                    <option>smooth night</option>
+                    <option>predator</option>
+                </select>
+            
+                <label for="textFontFamily">font family</label>
+                <input type="text" name="textFontFamily">
+
             </fieldset>
 
             
