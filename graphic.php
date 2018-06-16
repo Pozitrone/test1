b9e9d184261334a45cdf517e2124fce4b777e6b6
Vexx <36227512+Pozitrone@users.noreply.github.com>
2018-06-16 14:41:23 +0200

    Bugfixes in javaScript section.

--- graphic.php
+++ graphic.php
@@ -10,49 +10,51 @@
             
             function loadInputs()
             {
-                var i;
-                for (i=0; i<= parseInt(document.getElementById("inputAmount").innerHTML); i++)
+                var i=0;
+                document.getElementById("inputs").innerHTML="";
+                for (i=0; i<parseInt(document.getElementById("inputAmount").value); i++)
                     {
-                        document.getElementById("inputs").innerHTML.concat
+                        alert(i);
+                        document.getElementById("inputs").appendChild(document.createTextNode
                         (
-                            "<label for='inputType",i,"'>Input type</label>",
-                            "<select name='selectInputType",i,"'>",
-                                "<option>text</option>",
-                                "<option>number</option>",
-                                "<option>password</option>",
-                                "<option>date</option>",
-                                "<option>radio</option>",
-                                "<option>checkbox</option>",
-                                "<option>color</option>",
-                                "<option>email</option>",
-                            "</select>",
+                            "<label for='inputType"+i+"'>Input type</label>"+
+                            "<select name='selectInputType"+i+"'>"+
+                                "<option>text</option>"+
+                                "<option>number</option>"+
+                                "<option>password</option>"+
+                                "<option>date</option>"+
+                                "<option>radio</option>"+
+                                "<option>checkbox</option>"+
+                                "<option>color</option>"+
+                                "<option>email</option>"+
+                            "</select>"+
 
-                            "<label for='inputName",i,"'>Input name</label>",
-                            "<input type='text' name='inputName",i,"'>",
+                            "<label for='inputName"+i+"'>Input name</label>"+
+                            "<input type='text' name='inputName"+i+"'>"+
 
-                            "<label for='inputLabel",i,"'>Input label</label>",
-                            "<input type='text' name='inputLabel",i,"'>",
+                            "<label for='inputLabel"+i+"'>Input label</label>"+
+                            "<input type='text' name='inputLabel"+i+"'>"+
 
-                            "<label for='inputPlaceholder",i,"'>Input placeholder</label>",
-                            "<input type='text' name='inputPlaceholder",i,"'>", 
+                            "<label for='inputPlaceholder"+i+"'>Input placeholder</label>"+
+                            "<input type='text' name='inputPlaceholder"+i+"'>"+ 
 
-                            "<label for='inputValue",i,"'>Input value</label>",
-                            "<input type='text' name='inputValue",i,"'>",
+                            "<label for='inputValue"+i+"'>Input value</label>"+
+                            "<input type='text' name='inputValue"+i+"'>"+
 
-                            "<label for='switchRequired",i,"'>required</label>",
-                                "<label class='switch'>",
-                                "<input type='checkbox' name='switchRequired",i,"'>",
-                            "<span class='slider round'></span>",
-                            "</label>",
+                            "<label for='switchRequired"+i+"'>required</label>"+
+                                "<label class='switch'>"+
+                                "<input type='checkbox' name='switchRequired"+i+"'>"+
+                            "<span class='slider round'></span>"+
+                            "</label>"+
 
-                            "<label for='switchReadonly",i,"'>readonly</label>",
-                                "<label class='switch'>",
-                                "<input type='checkbox' name='switchReadonly",i,"'>",
-                            "<span class='slider round'></span>",
+                            "<label for='switchReadonly"+i+"'>readonly</label>"+
+                                "<label class='switch'>"+
+                                "<input type='checkbox' name='switchReadonly"+i+"'>"+
+                            "<span class='slider round'></span>"+
                             "</label>"
 
 
-                        );
+                        ));
                     }
                         
                 }
@@ -138,7 +140,7 @@
                 <input type="text" name="textResetValue">
             
                 <label for="numberInputAmount">input amount</label>
-                <input type="number" name="numberInputAmount" id="inputAmount">
+                <input type="number" name="numberInputAmount" id="inputAmount" onchange="loadInputs()">
             
                 <label for="textCssPath">css path</label>
                 <input type="text" name="textCssPath">
